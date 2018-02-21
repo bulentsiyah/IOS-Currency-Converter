97bd1a68687658f45265804a472f23407cc35cbd
Bulent SIYAH <info@bulentsiyah.com>
2018-02-21 16:51:26 +0300

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Bülent Siyah on 21.02.2018.
+//  Copyright © 2018 Bülent Siyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
